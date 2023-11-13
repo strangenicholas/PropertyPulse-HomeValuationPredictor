af1b148b411623f058c7f61cfa4f8813daa4091c
strangenicholas <nicholaswstrange@gmail.com>
2023-11-12 21:52:04 -0500

    Adjust urls

--- valueprediction_project/valueprediction_app/urls.py
+++ valueprediction_project/valueprediction_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path('predict/', views.predict_price, name='predict_price'),
+# ]
+
 urlpatterns = [
+    path('', views.default_view, name='default'),  # Define a view for the empty string path
     path('predict/', views.predict_price, name='predict_price'),
+    # Add more URL patterns specific to your app's views
 ]
--- valueprediction_project/valueprediction_project/urls.py
+++ valueprediction_project/valueprediction_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('prediction_app/', include('prediction_app.urls')),
+# ]
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('prediction_app/', include('prediction_app.urls')),
+    path('admin/', admin.site.urls),  # URL for the Django admin site
+    path('', include('valueprediction_app.urls')),  # Include app-specific URLs with an empty string
 ]
