6310ccbc8f16293a76487043f32666e96a3e3b13
strangenicholas <nicholaswstrange@gmail.com>
2023-11-14 21:39:53 -0500

    fix templates after merge

--- valueprediction_project/valueprediction_app/urls.py
+++ valueprediction_project/valueprediction_app/urls.py
@@ -3,16 +3,8 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
-        path('', views.login_view, name='login'),
-    # path('', views.predict_price, name='default'), 
-    path('accounts/profile/', views.predict_price, name='profile'),  # Replace YourProfileView with your actual view
-    
-=======
-
     path('', views.login_view, name='login'),
     # path('', views.predict_price, name='default'), 
     path('accounts/profile/', views.predict_price, name='profile'),  # Replace YourProfileView with your actual view
     path('predict/', views.predict_price, name='predict_price'),
->>>>>>> main
 ]
